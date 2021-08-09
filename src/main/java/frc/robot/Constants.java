37cf1def76ebd9297507c8b45830abff08980911
Mika <mikush.carmel@gmail.com>
2021-08-09 13:30:08 +0300

    added IDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,29 +8,46 @@ public final class Constants {
 
     public static final class DriveTrain {
 
-        public static final class TalonR{
+        public static final class TalonR {
+            public static final int TALONR_ID = 1;
             public static final boolean inverted = true;
         }
 
-        public static final class TalonL{
+        public static final class TalonL {
+            public static final int TALONL_ID = 2;
             public static final boolean inverted = true;
         }
 
-        public static final class VictorRF{
+        public static final class VictorRF {
+            public static final int VICTORRF_ID = 3;
             public static final boolean inverted = true;
         }
 
-        public static final class VictorRB{
+        public static final class VictorRB {
+            public static final int VICTORRB_ID = 4;
             public static final boolean inverted = true;
         }
 
-        public static final class VictorLF{
+        public static final class VictorLF {
+            public static final int VICTORLF_ID = 5;
             public static final boolean inverted = true;
         }
 
-        public static final class VictorLB{
+        public static final class VictorLB {
+            public static final int VICTORLB_ID = 6;
             public static final boolean inverted = true;
+        }  
+    }
+
+    public static final class Joysticks {
+
+        public static final class LeftJoystick  {
+            public static final int LEFT_JOYSTICK_PORT = 0;
         }
-        
+
+        public static final class RightJoystick  {
+            public static final int RIGHT_JOYSTICK_PORT = 1;
+        }
+
     }
 }
