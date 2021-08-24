6566866c8dc865e65e551f8a89a8a857c403a747
Mika <mikush.carmel@gmail.com>
2021-08-24 14:35:20 +0300

    removed unused methods

--- src/main/java/frc/robot/humanIO/Joysticks.java
+++ src/main/java/frc/robot/humanIO/Joysticks.java
@@ -4,27 +4,19 @@ import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.Constants;
 
 public class Joysticks {
-    
+
     private Joystick _leftJoystick, _rightJoystick;
 
     public Joysticks() {
-        this._leftJoystick = new Joystick(Constants.Joysticks.LeftJoystick.LEFT_JOYSTICK_PORT);
-        this._rightJoystick = new Joystick(Constants.Joysticks.RightJoystick.RIGHT_JOYSTICK_PORT);
+        this._leftJoystick = new Joystick(Constants.Joysticks.LeftJoystick.PORT);
+        this._rightJoystick = new Joystick(Constants.Joysticks.RightJoystick.PORT);
     }
 
-    public double getLeftY(){
+    public double getLeftY() {
         return this._leftJoystick.getY();
     }
 
-    public double getLeftX(){
-        return this._leftJoystick.getX();
-    }
-
-    public double getRightY(){
+    public double getRightY() {
         return this._rightJoystick.getY();
-    }
-
-    public double getRightX(){
-        return this._rightJoystick.getX();
     }
 }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -8,25 +8,22 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
-  
+
   private TalonSRX _talonR, _talonL;
   private VictorSPX _victorRF, _victorRB, _victorLF, _victorLB;
 
-  private DoubleSolenoid _shifter;
-
   public DriveTrain() {
 
-    this._talonR = new TalonSRX(Constants.DriveTrain.TalonL.TALONL_ID);
-    this._talonL = new TalonSRX(Constants.DriveTrain.TalonR.TALONR_ID);
-    this._victorRF = new VictorSPX(Constants.DriveTrain.VictorRF.VICTORRF_ID);
-    this._victorRB = new VictorSPX(Constants.DriveTrain.VictorRB.VICTORRB_ID);
-    this._victorLF = new VictorSPX(Constants.DriveTrain.VictorLF.VICTORLF_ID);
-    this._victorLB = new VictorSPX(Constants.DriveTrain.VictorLB.VICTORLB_ID);
+    this._talonR = new TalonSRX(Constants.DriveTrain.TalonL.ID);
+    this._talonL = new TalonSRX(Constants.DriveTrain.TalonR.ID);
+    this._victorRF = new VictorSPX(Constants.DriveTrain.VictorRF.ID);
+    this._victorRB = new VictorSPX(Constants.DriveTrain.VictorRB.ID);
+    this._victorLF = new VictorSPX(Constants.DriveTrain.VictorLF.ID);
+    this._victorLB = new VictorSPX(Constants.DriveTrain.VictorLB.ID);
 
     this._victorRF.follow(_talonR);
     this._victorRB.follow(_talonR);
@@ -42,25 +39,20 @@ public class DriveTrain extends SubsystemBase {
 
   }
 
-  public void setDistance(double distanceR, double distanceL) {
-    this._talonL.set(ControlMode.Position, distanceR);
-    this._talonL.set(ControlMode.Position, distanceL);
-  }
-
-  public void setRightPercent(double percentage){
+  private void setRightPercent(double percentage) {
     this._talonR.set(ControlMode.PercentOutput, percentage);
   }
 
-  public void setLeftPercent(double percentage){
+  private void setLeftPercent(double percentage) {
     this._talonL.set(ControlMode.PercentOutput, percentage);
   }
 
-  public void setBothPercent(double percentageL, double percentageR){
+  public void setPercent(double percentageL, double percentageR) {
     setRightPercent(percentageR);
     setLeftPercent(percentageL);
   }
 
-  public void setpercent(double percentage){
+  public void setPercent(double percentage) {
     setRightPercent(percentage);
     setLeftPercent(percentage);
   }
